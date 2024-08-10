639ff929622404fe8a2d967a6c22b31d324781a4
AnkitGurjar6633 <ankitgurjar6633@gmail.com>
2024-08-10 16:40:43 +0530

    Add project files.

---
+++ QueryBase.Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QueryBase.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
