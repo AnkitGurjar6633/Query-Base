7ea77aee928e7a62cd085a466710b77daff25a59
Ankit Gurjar <123095964+AnkitGurjar6633@users.noreply.github.com>
2024-09-20 19:29:32 +0530

    Delete QueryBase.Models directory

--- QueryBase.Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace QueryBase.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
